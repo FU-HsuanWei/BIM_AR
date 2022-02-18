b53aa23d888dda6036aa015aa5514bf9a0577e58
FU-HsuanWei <M0807591@o365.fcu.edu.tw>
2022-02-18 16:07:47 +0800

    20220218

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/build_GPS.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/build_BIM&interior_move.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
